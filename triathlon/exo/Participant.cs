1a68426b29dae849e8d82d715361b5387a0058a3
Swoodya <44286193+Swoodya@users.noreply.github.com>
2018-11-07 14:03:06 +0100

    Add files via upload

---
+++ triathlon/exo/Participant.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo
+{
+    class Participant
+    {
+        int id;
+        string nom;
+        DateTime dateInscription;
+        List<Resultat> lesResultats;
+
+        Participant(string nom, DateTime dateInscription)
+        {
+            this.nom = nom;
+            this.dateInscription = dateInscription;
+        }
+
+        public int GetId()
+        {
+            return this.id;
+        }
+
+        public string GetNom()
+        {
+            return this.nom;
+        }
+
+        public DateTime GetDateInscription()
+        {
+            return this.dateInscription;
+        }
+
+        private bool resultatPresent(Epreuve unEpreuve)
+        {
+            for(int i = 0; i < lesResultats.Count; i++)
+            {
+                if (lesResultats[i].GetEpreuve().GetNom() == unEpreuve.GetNom())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Add(Epreuve uneEpreuve, int tempsRealise)
+        {
+
+        }
+
+        public int GetTempsTotal()
+        {
+
+        }
+
+        public bool HorsDelai()
+        {
+
+        }
+    }
+}
---
+++ triathlon/exo/Tri.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo
+{
+    class Tri
+    {
+        string nature;
+        Epreuve natation;
+        Epreuve velo;
+        Epreuve course;
+        List<Participant> lesParticipants;
+
+        public Tri(string nature, int distanceNatation, int distanceVelo, int distanceCourse)
+        {
+            this.nature = nature;
+            this.natation = new Epreuve("natation", distanceNatation);
+            this.velo = new Epreuve("velo", distanceVelo);
+            this.course = new Epreuve("course", distanceCourse);
+            this.lesParticipants = new List<Participant>();
+        }
+
+        public string GetNature()
+        {
+            return this.nature;
+        }
+
+        public void AddParticipant(Participant unParticipant)
+        {
+            this.lesParticipants.Add(unParticipant);
+        }
+
+        public Epreuve GetEpreuve(string nomEpreuve)
+        {
+            switch (nomEpreuve)
+            {
+                case "natation":
+                    return this.natation;
+                case "velo":
+                    return this.velo;
+                default:
+                    return this.course;
+                    
+            }
+        }
+
+        public Participant GetParticipantById(int id)
+        {
+            foreach(Participant p in lesParticipants){
+                if (p.GetId() == id)
+                {
+                    return p;
+                }
+            }
+            return null;
+        }
+
+        public int GetDistanceTotale()
+        {
+            int DistanceTot;
+            DistanceTot = this.natation.GetDistance() + this.velo.GetDistance() + this.course.GetDistance();
+            return DistanceTot;
+
+        }
+
+        public List<Participant> GetLesTemps()
+        {
+            List<Participant> enCours = new List<Participant>();
+            foreach(Participant unParticipant in lesParticipants)
+            {
+                if(unParticipant HorsDelai== false)
+                {
+                    enCours.Add(unParticipant);
+                }
+            }
+            return enCours;
+        }
+    }
+}
